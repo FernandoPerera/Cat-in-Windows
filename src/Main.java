35d238a84f1132a3eaae8fff8e3ef3d7b8bb192b
Fernando <you@example.com>
2022-10-19 12:06:35 +0200

    Feature(Main.java) : Solicitamos el archivo, y comprobamos si existe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedimos la ruta del fichero
+
+        System.out.println("------------------------------------");
+        System.out.println("| Introduzca la ruta de un fichero |");
+        System.out.println("------------------------------------");
+        String path = scanner.nextLine();
+
+        File archivo = new File(path);
+
+        // Comprobamos si el fichero existe
+
+        if (!archivo.exists()) {
+            System.out.println("****************************************");
+            System.out.println("ERROR : el fichero introducido no existe");
+            System.out.println("****************************************");
+        } else {
+            System.out.println("El fichero si existe");
+        }
 
     }
 }
